5c75bae82bd43ad5dfaeb1cdc9cbbbd9ee16bf2d
matt dunlop <matthewjdunlop@outlook.com>
2020-04-30 18:55:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+//listen for request on port 3000, and as a callback function have the port listened on logged
+app.listen(3000, function() {
+    console.log(`Server running on port 3000`);
+  });
