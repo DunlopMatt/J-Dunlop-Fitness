4d86b2b0687e6d9f63c71ab477de20868d5f8d2c
matt dunlop <matthewjdunlop@outlook.com>
2020-05-25 16:49:45 +0100

    port

--- app.js
+++ app.js
@@ -116,7 +116,9 @@ app.post('/', function(req, res){
 
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
